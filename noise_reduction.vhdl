9adce1a12cb2a95a686c0766f5f37583341cfdef
Ivar Sörqvist <ivar.sorqvist@telia.com>
2021-03-29 20:09:48 +0200

    final version

--- noise_reduction.vhdl
+++ noise_reduction.vhdl
@@ -41,36 +41,50 @@ entity noise_reduction is
 end noise_reduction;
 
 architecture behaviourial of noise_reduction is
-signal test : std_logic := '0';    
+
+-- Limits on when to filter for "black" and "white" pixels
+signal black_filter : std_logic_vector(7 downto 0) := x"F0";
+signal white_filter : std_logic_vector(7 downto 0) := x"10";
+
+-- state machine
 type state_NR_type is (wait_start, read_first_vector, read_vector, last_vec);
 signal state_NR : state_NR_type := wait_start;
 
+-- keep track of last vector and the last pixel
+-- in the vector before that to make comparison
+-- P_{v(n-1)}[7] P[0] P[1] P[2] P[3] P[4] P[5] P[6] P[7] P_{v(n+1)}[0]  
 signal last_pixel_in_prev_x2_vector : std_logic_vector(7 downto 0); -- two lines ago
 signal previous_vector : std_logic_vector(63 downto 0); -- FFs for prev line
-
 type pixels_focus_type is array (9 downto 0) of std_logic_vector(7 downto 0);
 signal pixels_focus : pixels_focus_type;
+
+-- shifters to keep track when a vector is first or last in line
 signal first_vector_in_line : std_logic_vector(1 downto 0);
 signal last_vector_in_line : std_logic_vector(1 downto 0);
+-- shifter to keep track on when receiving eof signal to
+-- send out one on correct cycle
+signal eof_shifter : std_logic_vector(1 downto 0);
+-- shifter to keep track on if in was valid 2 cycles ago
+signal valid_in_shifter : std_logic_vector(1 downto 0);
+
+-- keep count on which vector in line and
+-- column wea are at
 signal line_vector_count : integer;
 signal column_count : integer;
 
 -- debugging
-signal pixel_left_signal : integer;
-signal pixel_right_signal : integer;
-signal pixel_filtered_signal : integer;
-signal line_width_signal : integer;
-signal line_vector_count_signal : integer;
+--signal pixel_left_signal : integer;
+--signal pixel_right_signal : integer;
+--signal pixel_filtered_signal : integer;
+--signal line_width_signal : integer;
+--signal line_vector_count_signal : integer;
 --
 
 begin
 
 NR_state_process : process (clk)
--- variable first_vector_in_line : boolean := false;
 
-variable width_count : integer := 0;
-variable height_vount : integer := 0;
-
+-- to filter pixel left and right is avereged to a "filtered" pixel
 variable pixel_left : integer;
 variable pixel_right : integer;
 variable pixel_filtered : integer;
@@ -80,7 +94,9 @@ variable pixel_filtered : integer;
 variable line_width : integer; 
 variable columns : integer;
 
-
+--to keep track on when
+variable sof_out_bool : boolean := false;
+variable eof_out_bool : boolean := false;
 
 begin
     if rst = '0' then -- Cannot start until rst is set to 1
@@ -94,12 +110,14 @@ begin
                     sof_out <= '0';
                     eof_out <= '0';
                     valid_out <= '0';
+                    eof_shifter <= b"00";
+                    
                     
                     if sof_in = '1' and valid_in = '1' then
                         state_NR <= read_first_vector;
                         --state <= x"1"; --debug
                         previous_vector <= data_in;
-                        width_count := width_count + 8;
+
                         first_vector_in_line <= b"10";
                         -- assuming more than 8 pixels
                         last_vector_in_line <= b"00";
@@ -109,108 +127,143 @@ begin
                         columns := to_integer(unsigned(image_height));
                         line_vector_count <= 1;
                         column_count <= 1;
+
+                        sof_out_bool := true;
+
+                        valid_in_shifter <= b"10";
                     else
                         first_vector_in_line <= b"00";
+                        valid_in_shifter <= b"00";
                     end if; -- else stay the same
                 when read_first_vector =>
-                    previous_vector <= data_in;
-                    last_pixel_in_prev_x2_vector <= previous_vector(7 downto 0);
-                    state_NR <= read_vector;
-                    first_vector_in_line <= '0' & first_vector_in_line(1);
-                    line_vector_count <= line_vector_count + 1;
-
-                    --dedubbing
-                    line_width_signal <= line_width;
-
-                    -- vector to be compared next state
-                    pixels_focus(8) <= previous_vector(63 downto 56);
-                    pixels_focus(7) <= previous_vector(55 downto 48);
-                    pixels_focus(6) <= previous_vector(47 downto 40);
-                    pixels_focus(5) <= previous_vector(39 downto 32);
-                    pixels_focus(4) <= previous_vector(31 downto 24);
-                    pixels_focus(3) <= previous_vector(23 downto 16);
-                    pixels_focus(2) <= previous_vector(15 downto 8);
-                    pixels_focus(1) <= previous_vector(7 downto 0);
-                    pixels_focus(0) <= data_in(63 downto 56);
-                when read_vector =>
-                    if column_count = columns and line_vector_count = line_width then
-                        -- finish and wait for another one!
-                        state_NR <= wait_start;
-                        eof_out <= '1';
-                    end if;
-
-                    if line_vector_count = line_width then
-                        first_vector_in_line <= '1' & first_vector_in_line(0);
-                        line_vector_count <= 1;
-                        column_count <= column_count + 1;
-                    else
+                    -- stay until valid_in = 1
+                    if valid_in = '1' then
+                        -- set previous vector to in data
+                        previous_vector <= data_in;
+                        last_pixel_in_prev_x2_vector <= previous_vector(7 downto 0);
+                        
+                        -- shift first vector tracking and add on counter
                         first_vector_in_line <= '0' & first_vector_in_line(1);
                         line_vector_count <= line_vector_count + 1;
 
+                        --dedubbing
+                        --line_width_signal <= line_width;
+
+                        -- vector to be compared next state
+                        for i in 1 to 8 loop
+                            pixels_focus(i) <= previous_vector((i*8 - 1) downto (i-1)*8);
+                        end loop;
+                        pixels_focus(0) <= data_in(63 downto 56);
                         
+                        -- next state
+                        state_NR <= read_vector;
+
+                        valid_in_shifter <= '1' & valid_in_shifter(1);
+                    --else
+                    --    valid_in_shifter <= '0' & valid_in_shifter(1);
                     end if;
-                    -- debugging
-                    line_vector_count_signal <= line_vector_count;
+                when read_vector =>
+                    if valid_in = '1' then
+                        -- read in data
 
-                    previous_vector <= data_in;
-                    last_pixel_in_prev_x2_vector <= previous_vector(7 downto 0);
+                        -- Set for future read_line
+                        previous_vector <= data_in;
+                        valid_in_shifter <= '1' & valid_in_shifter(1);
+                    else
+                        valid_in_shifter <= '0' & valid_in_shifter(1);
+                    end if;
 
-                    -- vector to be compared next state
-                    pixels_focus(9) <= last_pixel_in_prev_x2_vector;
-                    pixels_focus(8) <= previous_vector(63 downto 56);
-                    pixels_focus(7) <= previous_vector(55 downto 48);
-                    pixels_focus(6) <= previous_vector(47 downto 40);
-                    pixels_focus(5) <= previous_vector(39 downto 32);
-                    pixels_focus(4) <= previous_vector(31 downto 24);
-                    pixels_focus(3) <= previous_vector(23 downto 16);
-                    pixels_focus(2) <= previous_vector(15 downto 8);
-                    pixels_focus(1) <= previous_vector(7 downto 0);
-                    pixels_focus(0) <= data_in(63 downto 56);
-                    for i in 1 to 8 loop
-                        -- eof_out <= '0';
-                        if pixels_focus(i) >= x"F0" or pixels_focus(i) <= x"10" then
-                            -- Check if pixel is "black" or "light" =>
-                            -- filter
-                            --eof_out <= '1'; -- debugging
-                            --report "hello";
-                            if first_vector_in_line(0) = '1' and i = 8 then -- highest index to the left
-                                --report "hello1";
-                                -- no pixels to the left when first vector in line => 
-                                -- set to same as pixel to the right
-                                data_out((i*8 - 1) downto (i-1)*8) <= pixels_focus(i-1);
-                            elsif last_vector_in_line(0) = '1' and i = 1 then
-                                --report "hello2";
-                                -- no pixel to the right of when last vector in line =>
-                                -- set to same pixel as to the left
-                                data_out((i*8 + 7) downto i*8) <= pixels_focus(i+1);
-                            else
-                                --report "hello3";
-                                -- filter the data by averaging with the two closest pixels
-                                -- to the right and left
-                                -- Always round of to lowest integer (20 + 21) / 2 = 40.5 => 40
-                                pixel_left := to_integer(unsigned(pixels_focus(i-1)));
-                                pixel_right := to_integer(unsigned(pixels_focus(i+1)));
-                                pixel_filtered := (pixel_left + pixel_right) / 2;
+                    if valid_in_shifter(1) = '1' then
+                        last_pixel_in_prev_x2_vector <= previous_vector(7 downto 0);
 
-                                -- debugging
-                                pixel_left_signal <= pixel_left;
-                                pixel_right_signal <= pixel_right;
-                                pixel_filtered_signal <= pixel_filtered;
-                                -- 
+                        -- vector to be compared next state
+                        pixels_focus(9) <= last_pixel_in_prev_x2_vector;
+                        for i in 1 to 8 loop
+                            pixels_focus(i) <= previous_vector((i*8 - 1) downto (i-1)*8);
+                        end loop;
+                        pixels_focus(0) <= data_in(63 downto 56);
 
-                                data_out(((i-1)*8 + 7) downto (i-1)*8) <= std_logic_vector(to_unsigned(pixel_filtered, 8));
-                            end if; -- first, middle or last pixel in line
-                        else 
-                            -- If passing filter => keep same value
-                            data_out(((i-1)*8 + 7) downto (i-1)*8) <= pixels_focus(i);
-                        end if; -- pixels_focus(i) >= ...
-                        valid_out <= '1';
-                        if first_vector_in_line(0) = '1' then
+                        if line_vector_count = line_width then
+                            -- Last vector in line => next vector is gonna be first in its line!
+                            first_vector_in_line <= '1' & first_vector_in_line(1);
+                            line_vector_count <= 1;
+                            column_count <= column_count + 1;
+                        else
+                            first_vector_in_line <= '0' & first_vector_in_line(1);
+                            line_vector_count <= line_vector_count + 1;                        
+                        end if;
+
+                        if line_vector_count = line_width - 1 then
+                            -- next vector is gonna be last in its line!
+                            last_vector_in_line <= '1' & last_vector_in_line(1);
+                        else
+                            last_vector_in_line <= '0' & last_vector_in_line(1);
+                        end if;
+                    end if;
+
+                    if valid_in_shifter(0) = '1' then
+                        -- only calculate on data that was correct read;
+                        if column_count = columns and line_vector_count = line_width then
+                            -- Finished with the picture! 
+                            state_NR <= wait_start;
+                            eof_out <= '1';
+                        end if;
+
+                        -- debugging
+                        --line_vector_count_signal <= line_vector_count;
+                        for i in 1 to 8 loop
+                            if pixels_focus(i) >= black_filter or pixels_focus(i) <= white_filter then
+                                -- Check if pixel is too dark or bright =>
+                                -- filter with nearby pixels
+                                if first_vector_in_line(0) = '1' and i = 8 then -- highest index to the left
+                                    -- no pixels to the left when first vector in line => 
+                                    -- set to same as pixel to the right
+                                    data_out((i*8 - 1) downto (i-1)*8) <= pixels_focus(i-1);
+                                elsif last_vector_in_line(0) = '1' and i = 1 then
+                                    -- no pixel to the right of when last vector in line =>
+                                    -- set to same pixel as to the left
+                                    data_out((i*8 - 1) downto (i-1)*8) <= pixels_focus(i+1);
+                                else
+                                    -- filter the data by averaging with the two closest pixels
+                                    -- to the right and left
+                                    -- Always round of to lowest integer (20 + 21) / 2 = 40.5 => 40
+                                    pixel_left := to_integer(unsigned(pixels_focus(i-1)));
+                                    pixel_right := to_integer(unsigned(pixels_focus(i+1)));
+                                    pixel_filtered := (pixel_left + pixel_right) / 2;
+
+                                    -- debugging
+                                    --pixel_left_signal <= pixel_left;
+                                    --pixel_right_signal <= pixel_right;
+                                    --pixel_filtered_signal <= pixel_filtered;
+                                    -- 
+
+                                    data_out(((i-1)*8 + 7) downto (i-1)*8) <= std_logic_vector(to_unsigned(pixel_filtered, 8));
+                                end if; -- first, middle or last pixel in line
+                            else 
+                                -- If passing filter => keep same value
+                                data_out(((i-1)*8 + 7) downto (i-1)*8) <= pixels_focus(i);
+                            end if; -- pixels_focus(i) >= ...
+                        end loop;
+
+                        -- set sof_out;
+                        if first_vector_in_line(0) = '1' and sof_out_bool then
+                            sof_out_bool := false;
                             sof_out <= '1';
                         else
                             sof_out <= '0';
                         end if;
-                    end loop;
+
+                        -- set eof_out
+                        eof_shifter <= eof_in & eof_shifter(1);
+                        if eof_shifter(0) = '1' then
+                            eof_out <= '1';
+                        end if;
+
+                        -- assume always valid out if valid in;
+                        valid_out <= '1';
+                    else
+                        valid_out <= '0';
+                    end if; -- valid in 
 
                 when others => 
                     -- Should never happen
--- noise_reduction_tb.vhdl
+++ noise_reduction_tb.vhdl
@@ -2,6 +2,10 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+-- Solution of TB
+-- Have one in file read and then one check file to check for correct
+-- outputs of data_out, other values such as sof_out and eof_out
+-- is checked by simple assert values.
 -- for reading test file
 use STD.textio.all;
 use ieee.STD_LOGIC_TEXTIO.all;
@@ -27,9 +31,6 @@ component noise_reduction is
         sof_out         : out std_logic; -- '1' during first valid data in image
         eof_out         : out std_logic; -- '1' during last valid data in image
         data_out        : out std_logic_vector(63 downto 0) -- 8 pixels
-
-        -- debugging
-        --state           : out std_logic_vector(3 downto 0)
     );
 end component;
 
@@ -45,13 +46,11 @@ signal tb_valid_out     : std_logic;
 signal tb_sof_out       : std_logic;
 signal tb_eof_out       : std_logic;
 signal tb_data_out      : std_logic_vector(63 downto 0);
--- debugging
---signal tb_state         : std_logic_vector(3 downto 0);
 
 file file_IMAGE : text;
---signal image_data       : std_logic_vector(63 downto 0) := X"FFFFFFFFFFFFFFFF";
 
 -- Convert std_logic_vector to string, for debugging
+-- borrowed from online
 function to_hstring (SLV : std_logic_vector) return string is
     variable L : LINE;
   begin
@@ -61,39 +60,38 @@ end function to_hstring;
 
 -- Convert string char to std_logic_vector. 
 -- Used for reading input file
-FUNCTION hexchar2bin (hex: character) RETURN std_logic_vector IS
-VARIABLE result : std_logic_vector (3 downto 0);
-BEGIN
-CASE hex IS
-    WHEN '0' =>     result := "0000";
-    WHEN '1' =>     result := "0001";
-    WHEN '2' =>     result := "0010";
-    WHEN '3' =>     result := "0011";
-    WHEN '4' =>     result := "0100";
-    WHEN '5' =>     result := "0101";
-    WHEN '6' =>     result := "0110";
-    WHEN '7' =>     result := "0111";
-    WHEN '8' =>     result := "1000";
-    WHEN '9' =>     result := "1001";
-    WHEN 'A'|'a' => result := "1010";
-    WHEN 'B'|'b' => result := "1011";
-    WHEN 'C'|'c' => result := "1100";
-    WHEN 'D'|'d' => result := "1101";
-    WHEN 'E'|'e' => result := "1110";
-    WHEN 'F'|'f' => result := "1111";
-    WHEN 'X'|'x' => result := "XXXX";
-    WHEN others =>  NULL;
-END CASE;
-RETURN result;
-END;
+-- borrowed from online
+function hexchar2bin (hex: character) return std_logic_vector IS
+variable result : std_logic_vector (3 downto 0);
+begin
+case hex is
+    when '0' =>     result := "0000";
+    when '1' =>     result := "0001";
+    when '2' =>     result := "0010";
+    when '3' =>     result := "0011";
+    when '4' =>     result := "0100";
+    when '5' =>     result := "0101";
+    when '6' =>     result := "0110";
+    when '7' =>     result := "0111";
+    when '8' =>     result := "1000";
+    when '9' =>     result := "1001";
+    when 'A'|'a' => result := "1010";
+    when 'B'|'b' => result := "1011";
+    when 'C'|'c' => result := "1100";
+    when 'D'|'d' => result := "1101";
+    when 'E'|'e' => result := "1110";
+    when 'F'|'f' => result := "1111";
+    when 'X'|'x' => result := "XXXX";
+    when others =>  NULL;
+end case;
+return result;
+end;
 
-signal test_integer : integer;
-signal test_std : std_logic_vector(7 downto 0) := x"FF";
-
+-- vector used to compare with output vector
 signal check_vector : std_logic_vector(63 downto 0);
 
 begin
-
+    -- Unit Under Test
     UUT : component noise_reduction
         port map (
             rst             => tb_rst,
@@ -111,32 +109,33 @@ begin
         );
 
     
-    tb_clk <= NOT tb_clk after 5 ns; -- 100 Mhz
-    tb_rst <= '1' after 20 ns;
-
-    test_integer <= to_integer(unsigned(test_std));
+    tb_clk <= NOT tb_clk after 5 ns; -- Set clock to 100 Mhz
+    tb_rst <= '1' after 20 ns; -- start by "unresetting"
 
     -- try on a 24 pixel x 24 pixel picture => 
     tb_image_height <= b"00" & x"18"; -- 24 pixels of each 1 byte
     tb_image_width <= b"00" & x"18"; -- 24 pixels
 
     read_in_file: process
-        constant file_in_name   : string := "test_input.txt";
-        file file_in_pointer    : text;
-        variable line_in_content   : string(1 to 23); -- in line
-        variable line_in_num       : line;
-        variable filestatus_in     : file_open_status;
+        constant file_in_name           : string := "test_input.txt";
+        file file_in_pointer            : text;
+        variable line_in_content        : string(1 to 23); -- in line
+        variable line_in_num            : line;
+        variable filestatus_in          : file_open_status;
 
-        constant file_check_name   : string := "test_check.txt";
-        file file_check_pointer    : text;
-        variable line_check_content   : string(1 to 23); -- in line
-        variable line_check_num       : line;
-        variable filestatus_check     : file_open_status;
+        constant file_check_name        : string := "test_check.txt";
+        file file_check_pointer         : text;
+        variable line_check_content     : string(1 to 23); -- in line
+        variable line_check_num         : line;
+        variable filestatus_check       : file_open_status;
 
-        variable counter    : integer := 0;
+        variable counter                : integer := 0;
 
-        variable start          : boolean := false;
+        variable start                  : boolean := false;
     begin
+        tb_eof_in <= '0';
+        tb_sof_in <= '0';
+        tb_valid_in <= '0';
         tb_data_in <= x"0000000000000000";
         wait until tb_rst = '1'; -- start first when rst says its okay
 
@@ -165,22 +164,19 @@ begin
                         & hexchar2bin(line_in_content(16)) & hexchar2bin(line_in_content(17))
                         & hexchar2bin(line_in_content(19)) & hexchar2bin(line_in_content(20))
                         & hexchar2bin(line_in_content(22)) & hexchar2bin(line_in_content(23));
-                if line_in_content = "FF BF CA CB CC CE CD CF" then
+                if endfile(file_in_pointer) then
                     tb_eof_in <= '1';
                 end if;
             end if;
             
             if counter > 3 then
-                if line_check_content = "AB AB AB AD AE AF BA BC" then
-                    assert tb_sof_out = '1' report "sof_out not 1!" severity FAILURE; 
-                end if;
                 assert tb_eof_out = '0' report "eof_out is not supposed to be 1" severity FAILURE;
                 -- start comparing output with expected output
                 readline(file_check_pointer, line_check_num);
                 read(line_check_num, line_check_content);
                 --report real'image(line_check_content);
                 --report line_check_content(1 to 23);
-                
+
                 check_vector(63 downto 0) <= hexchar2bin(line_check_content(1)) & hexchar2bin(line_check_content(2))
                     & hexchar2bin(line_check_content(4)) & hexchar2bin(line_check_content(5))
                     & hexchar2bin(line_check_content(7)) & hexchar2bin(line_check_content(8))
@@ -190,43 +186,47 @@ begin
                     & hexchar2bin(line_check_content(19)) & hexchar2bin(line_check_content(20))
                     & hexchar2bin(line_check_content(22)) & hexchar2bin(line_check_content(23));
 
-                wait until falling_edge(tb_clk);
-                --report to_hstring(tb_data_out);
-                --report to_hstring(check_vector);
-                -- quit when reaching last test vector
-                --assert check_vector /= x"0000000000000000" report "Last test vector reached => quitting"
-                --                                            severity FAILURE;
-                
-                assert tb_data_out = check_vector report integer'image(counter - 3) &
+                wait until falling_edge(tb_clk); -- Make test first in mid clk-cycle
+
+                if tb_valid_out = '0' then
+                    report "valid_out = 0, skipping testing this line of check file with output";
+                    -- Data out should be same as previous.
+                else
+                    assert tb_data_out = check_vector report integer'image(counter - 3) &
                                                         " __ Data out: " & to_hstring(tb_data_out) & LF &
                                                         "                   " &
-                "                                            Check out: " & to_hstring(check_vector) 
+                    "                                            Check out: " & to_hstring(check_vector) 
                                                         severity WARNING;
-                
-            end if;
+                end if;
 
+                if counter = 4 then
+                    assert tb_sof_out = '1' report "sof_out not 1!" severity FAILURE;
+                else
+                    assert tb_sof_out = '0' report "sof_out not 0!" severity FAILURE;
+                end if;
+
+                --assert tb_valid_out = '1' report "valid out not 1" severity FAILURE;                
+            end if;
 
             if not start then
                 tb_sof_in <= '1'; -- first input
                 start := true;
             end if;
-            tb_valid_in <= '1'; -- Valid input
 
-            --assert tb_sof_out = '0' report "SOF out = 1" severity WARNING;
-
-            --report to_hstring(hexchar2bin(line_in_content(4)));
-            --report to_hstring(tb_data_in);
-            --report to_hstring(tb_data_out);
-            --report "STATE: " & to_hstring(tb_state);
-            --report image_data;
-
-
+            -- testing delaying one cycle with valid input
+            -- In input file this is added as a random line at 6
+            -- since out
+            if counter = 6 then
+                tb_valid_in <= '0';
+            else 
+                tb_valid_in <= '1';
+            end if;
 
         end loop;
         file_close(file_in_pointer);
         file_close(file_check_pointer);
         
-        assert tb_eof_out = '1' report "eof_out is 1!" severity WARNING;
+        assert tb_eof_out = '1' report "eof_out is not 1!" severity FAILURE;
 
         wait for 100 ns;
         -- End test
